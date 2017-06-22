25611b646aa8c32843ff184e98b0a617a2bef7a1
yuyuane <yuyuane@163.com>
2017-06-22 15:11:20 +0800

    添加获取随机数方法

--- application/common.php
+++ application/common.php
@@ -1,12 +1,36 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 流年 <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 // 应用公共文件
+function getRandTime(){
+    $time = time();
+    $date = @date('Y-m-d');
+    switch($time){
+        case $time< @strtotime($date." 8:00:00") && $time > @strtotime($date." 00:00:00"):
+            $t = rand(8000,8999);
+            break;
+        case $time < @strtotime($date." 10:00:00") && $time > @strtotime($date." 8:00:00"):
+            $t = rand(1000,1999);
+            break;
+        case $time < @strtotime($date." 12:00:00") && $time > @strtotime($date." 10:00:00"):
+            $t = rand(2000,2999);
+            break;
+        case $time < @strtotime($date." 14:00:00") && $time > @strtotime($date." 12:00:00"):
+            $t = rand(3000,3999);
+            break;
+        case $time < @strtotime($date." 18:00:00") && $time > @strtotime($date." 14:00:00"):
+            $t = rand(4000,4999);
+            break;
+        case $time < @strtotime($date." 20:00:00") && $time > @strtotime($date." 18:00:00"):
+            $t = rand(5000,5999);
+            break;
+        case $time < @strtotime($date." 22:00:00") && $time > @strtotime($date." 20:00:00"):
+            $t = rand(6000,6999);
+            break;
+        case $time < @strtotime($date." 24:00:00") && $time > @strtotime($date." 22:00:00"):
+            $t = rand(7000,7999);
+            break;
+        default :
+            $t = rand(500,999);
+    }
+    return $t;
+}
