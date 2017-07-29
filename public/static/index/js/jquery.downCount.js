3e74c6a5d07e5b4f658237f152552b7c20e0a26a
yuyuane <yuyuane@163.com>
2017-07-29 11:15:15 +0800

    提交页面

--- public/static/index/js/jquery.downCount.js
+++ public/static/index/js/jquery.downCount.js
@@ -1,104 +1,125 @@
-/**
- * downCount: Simple Countdown clock with offset
- * Author: Sonny T. <hi@sonnyt.com>, sonnyt.com
+/*
+ * jQuery Form Plugin:jquery.downCount.js
+ * 倒计时插件
+ *
+ * author:  damys
+ * version: 1.1
+ * url:     https://github.com/damys/lab-js/tree/master/downCount
+ * Date:    2016/12
  */
 
+
 (function ($) {
-
     $.fn.downCount = function (options, callback) {
-        var settings = $.extend({
-                date: null,
-                offset: null
-            }, options);
+        var configs = $.extend({
+            date: null,
+            offset:8,     // 时区+8
+            milliShow:0   // 是否显示毫秒 ， 注：较消耗内存， 不建议使用
+        }, options);
 
-        // Throw error if date is not set
-        if (!settings.date) {
+        if (!configs.date) {
             $.error('Date is not defined.');
         }
 
-        // Throw error if date is set incorectly
-        if (!Date.parse(settings.date)) {
+        // 如果日期格式设置错误，抛出错误
+        if (!Date.parse(configs.date)) {
             $.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
         }
 
-        // Save container
-        var container = this;
-
-        /**
-         * Change client's local date to match offset timezone
-         * @return {Object} Fixed Date object.
-         */
         var currentDate = function () {
-            // get client's current date
             var date = new Date();
 
             // turn date to utc
             var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
-
-            // set new Date object
-            var new_date = new Date(utc + (3600000*settings.offset))
+            var new_date = new Date(utc + (3600000*configs.offset))
 
             return new_date;
+
+            // 更新：location time
+            // return new Date(date.getTime());
         };
 
-        /**
-         * Main downCount function that calculates everything
-         */
-        function countdown () {
-            var target_date = new Date(settings.date), // set target date
-                current_date = currentDate(); // get fixed current date
+        // 保存容器对象
+        var container = this;
 
-            // difference of dates
+        // 倒计时
+        function countdown () {
+            var target_date = new Date(configs.date), // 设置目标日期
+                current_date = currentDate();         // 获取静态当前日期
+
+            // 计算不同日期
             var difference = target_date - current_date;
 
-            // if difference is negative than it's pass the target date
+            // 当日期差小于0时，清除
             if (difference < 0) {
                 // stop timer
                 clearInterval(interval);
 
-                if (callback && typeof callback === 'function') callback();
-
+                if (callback && typeof callback === 'function') {
+                    callback();
+                }
                 return;
             }
 
-            // basic math variables
+
             var _second = 1000,
                 _minute = _second * 60,
                 _hour = _minute * 60,
                 _day = _hour * 24;
 
-            // calculate dates
+            // 日期数据
             var days = Math.floor(difference / _day),
                 hours = Math.floor((difference % _day) / _hour),
                 minutes = Math.floor((difference % _hour) / _minute),
                 seconds = Math.floor((difference % _minute) / _second);
 
-                // fix dates so that it will show two digets
-                days = (String(days).length >= 2) ? days : '0' + days;
-                hours = (String(hours).length >= 2) ? hours : '0' + hours;
-                minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-                seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+            // fix dates so that it will show two digets
+            days = (String(days).length >= 2) ? days : '0' + days;
+            hours = (String(hours).length >= 2) ? hours : '0' + hours;
+            minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+            seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
 
-            // based on the date change the refrence wording
+            // 当前时间类型
             var ref_days = (days === 1) ? 'day' : 'days',
                 ref_hours = (hours === 1) ? 'hour' : 'hours',
                 ref_minutes = (minutes === 1) ? 'minute' : 'minutes',
                 ref_seconds = (seconds === 1) ? 'second' : 'seconds';
 
-            // set to DOM
+            // 设置DOM 时间数据
             container.find('.days').text(days);
             container.find('.hours').text(hours);
             container.find('.minutes').text(minutes);
             container.find('.seconds').text(seconds);
 
+            // 设置DOM 时间类型数据
             container.find('.days_ref').text(ref_days);
             container.find('.hours_ref').text(ref_hours);
             container.find('.minutes_ref').text(ref_minutes);
             container.find('.seconds_ref').text(ref_seconds);
-        };
-        
-        // start
-        var interval = setInterval(countdown, 1000);
-    };
+        }
 
+        // 开始设置计数器：1秒调用1次
+        var interval = setInterval(countdown, 1000);
+
+
+        // 是否显示毫秒， 注：较消耗内存， 不建议使用
+        if(configs.milliShow){
+
+            var ref_Milliseconds = 0;
+
+            function Millisecond() {
+                ref_Milliseconds++;
+
+                // 只显示两位
+                if(ref_Milliseconds > 100){
+                    ref_Milliseconds = 0;
+                    return;
+                }
+                container.find('.Milliseconds').text(ref_Milliseconds);
+            }
+
+            // 开始设置计数器， 1000分之1秒调用一次
+            setInterval(Millisecond, 1);
+        }
+    };
 })(jQuery);
