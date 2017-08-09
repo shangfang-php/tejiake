0a85341e035e5bd3291badeb482e712dcffc7bee
闫应门 <xingyunxlm@163.com>
2017-08-09 15:34:13 +0800

    修改userCommon文件

--- application/index/controller/UserCommon.php
+++ application/index/controller/UserCommon.php
@@ -6,14 +6,13 @@ use think\Db;
 class UserCommon extends Common{
     public function _initialize(){
     	parent::_initialize();
+        /** 用户未登录则跳转到登录界面 */
+        if(!self::$login_user){
+            $this->redirect('/user_login');
+        }
     	$this->assign('goods_type', self::$goods_type);
 		$uid = self::$login_user['id'];
 		$collect_count = Db::name('goods_collect')->where(['uid'=>$uid,'is_spread'=>0])->count();
 		$this->assign('collect_count', $collect_count);
-    	
-    	/** 用户未登录则跳转到登录界面 */
-    	if(!self::$login_user){
-    		$this->redirect('/user_login');
-    	}
     }
 }
